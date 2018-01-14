04be57c08e5fd435832b377e816eb8ea3fe30617
kkxiaojun <985531883@qq.com>
2018-01-14 23:28:52 +0800

    添加拖拽事件demo

--- vue-form/src/main.js
+++ vue-form/src/main.js
@@ -4,8 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import '@/common/less/index.less'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
